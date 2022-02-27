ea50fdf959e068ede0698136f71aded68d69a7aa
vaishakh3627 <96886376+vaishakh3627@users.noreply.github.com>
2022-02-27 15:21:26 +0530

    Added dummy token

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -16,7 +16,11 @@ const AuthProvider = (props) => {
       isLoading: false,
     });
 
+  const tokenDummy =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjE1MWRmOWVhODA2MzQ2OGIwMTBjNzRjIiwiaWF0IjoxNjQ1ODgzNTIxLCJleHAiOjE2NDg0NzU1MjF9.9Z2XDb-lmLWfq2Jza-er7x5Ludvsueoa7SFmeYFFe64";
+
   useEffect(() => {
+    localStorage.setItem("econ-user-key", JSON.stringify(tokenDummy));
     let token = JSON.parse(localStorage.getItem("ecom-user-key"));
     if (token !== user.token) updateUser(token, "admin");
     else updateUser(null);
